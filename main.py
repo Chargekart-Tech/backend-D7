9d0a6ce8f33e6f0b58b7671c9c04dfc80b570840
Bhav Beri <bhavberi@gmail.com>
2023-05-15 17:13:22 +0530

    Added Basic Backend Structure

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from os import getenv
+# from routers import users
+
+DEBUG = getenv("BACKEND_DEBUG", "False").lower() in ("true", "1", "t")
+
+# Create a new FastAPI instance
+# FastAPI App
+if DEBUG:
+    app = FastAPI(
+        debug=DEBUG,
+        title="ChargeKart",
+        description="ChargeKart Backend",
+        root_path="/api"
+    )
+else:
+    app = FastAPI(
+        debug=DEBUG,
+        title="ChargeKart",
+        description="ChargeKart Backend",
+        docs_url=None,
+        redoc_url=None,
+        root_path="/api"
+    )
+
+# Backend Index Page - For checking purposes
+@app.get("/")
+async def index():
+    return {"message": "Backend Running!!"}
+
+# Mount the user router on the "/auth" path
+# app.include_router(users.router, prefix="/auth", tags=["auth"])
