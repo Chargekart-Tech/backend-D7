e3219e363171a59d9b4c18d9c6acb044cc1c51d8
Aditya Raghuvanshi <raghuvanshiadityapiloy@gmail.com>
2023-06-14 03:19:54 +0530

    Synced with frontend

--- models/locations.py
+++ models/locations.py
@@ -1,26 +1,33 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
+
 class Coordinates(BaseModel):
     latitude: float
     longitude: float
 
 # Location Model
+
+
 class Location(BaseModel):
     locid: str
-    name: str
+    title: str
+    description: str
     coordinates: Coordinates
     city: Optional[str] = None
     state: Optional[str] = None
     country: Optional[str] = "India"
     pincode: Optional[int] = None
 
+
 class ManyLocationsResponse(BaseModel):
     locations: List[Location] | None
 
+
 class LocationInput(BaseModel):
     locid: Optional[str] = None
-    name: str
+    title: str
+    description: str
     coordinates: Coordinates
     city: Optional[str] = None
     state: Optional[str] = None
