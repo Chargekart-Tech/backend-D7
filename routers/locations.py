eba3ed1692d18e8f7c1721cb080426e0dc17ea69
Aditya Raghuvanshi <raghuvanshiadityapiloy@gmail.com>
2023-06-14 03:19:05 +0530

    Synced with frontend

--- routers/locations.py
+++ routers/locations.py
@@ -7,6 +7,7 @@ from routers.users import check_current_user, get_current_user
 
 router = APIRouter()
 
+
 def get_parking_location_by_id(id: str):
     try:
         pl = db.locations.find_one({"locid": id}, {"_id": 0})
@@ -15,16 +16,20 @@ def get_parking_location_by_id(id: str):
         return None
     except Exception:
         return None
-    
+
 # Endpoint to get all locations
+
+
 @router.get("/all")
 def get_locations():
     locations = list(db.locations.find({}, {"_id": 0}))
     if len(locations):
-        return ManyLocationsResponse(locations = locations)
+        return ManyLocationsResponse(locations=locations)
     return None
 
 # Endpoint to get a particular location details
+
+
 @router.get("/location/{locid}")
 # def get_details_locid(locid: int, _: str = Depends(get_current_user)):
 def get_details_locid(locid: str):
@@ -35,21 +40,23 @@ def get_details_locid(locid: str):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Location not found!")
 
-#Endpoint to add a New Location
+# Endpoint to add a New Location
+
+
 @router.post("/new-location", status_code=status.HTTP_201_CREATED)
 def add_new_location(location: LocationInput, response: Response):
-    existing_location = db.locations.find_one({"name": location.name,
+    existing_location = db.locations.find_one({"title": location.title,
                                                "latitude": location.coordinates.latitude,
                                                "longitude": location.coordinates.longitude},
                                               {"_id": 0})
     if existing_location:
         response.status_code = status.HTTP_409_CONFLICT
         return {"ERROR": "Exact location already exists.", "locid": existing_location["locid"]}
-    
-    if(location.pincode and len(str(location.pincode)) != 6):
+
+    if (location.pincode and len(str(location.pincode)) != 6):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Invalid Pin Code!")
-    
+
     location.locid = str(uuid4())
     db.locations.insert_one(location.dict())
     return {"locid": location.locid}
